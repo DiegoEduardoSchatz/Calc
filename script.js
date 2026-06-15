d1fe83e95c9fa86dc3a50cd4b7f8ddf593777320
DiegoEduardoSchatz <69731465+DiegoEduardoSchatz@users.noreply.github.com>
2026-06-15 18:28:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 function calcularDesconto() {
-    // Captura os valores dos inputs
+
     const precoInput = document.getElementById('preco').value;
     const descontoInput = document.getElementById('desconto').value;
 
-    // Converte para formato de número com ponto flutuante
+
     const preco = parseFloat(precoInput);
     const desconto = parseFloat(descontoInput);
 
-    // Validação básica para garantir que são números válidos
-    if (isNaN(preco) || isNaN(desconto)) {
+
+    if ((preco) > 0) {
+    }
+    else {
         alert("Por favor, preencha ambos os campos com números válidos.");
         return;
     }
 
-    // Cálculo do valor do desconto e do preço final
+    if ((desconto) >= 0) {
+    }
+    else {
+        alert("Por favor, preencha ambos os campos com números válidos.");
+        return;
+    }
+
+    if ((desconto) <= 100) {
+    }
+    else {
+        alert("Por favor, preencha ambos os campos com números válidos.");
+        return;
+    }
+
     const valorDesconto = preco * (desconto / 100);
     const precoFinal = preco - valorDesconto;
 
-    // Exibe o resultado formatado em moeda brasileira (R$)
-    /*document.getElementById('resultado').innerText = `Valor do Desconto: R$ ${valorDesconto.toFixed(2)} | Preço Final: R$ ${precoFinal.toFixed(2)}`;*/
-
-        alert("O valor final com " + descontoInput + "% de desconto é R$ " + precoFinal.toFixed(2));
+    
+    alert("O valor final com " + descontoInput + "% de desconto é R$ " + precoFinal + " e valor de desconto é R$ " + valorDesconto.toFixed(2));
+    
+    console.log("Desconto %: " + descontoInput);
+    console.log("Preço R$: " + precoInput);
+    console.log("Valor desconto R$: " + valorDesconto);
 }
